9923063940df7dc6aedfb09a080d0fc697de1d3b
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2020-05-27 23:41:59 -0700

    Update memory.c

--- memory.c
+++ memory.c
@@ -12,3 +12,14 @@ void main () // required main
 {
     funct1(1); // calls main with argument
 } // end main
+
+/*
+Doesn't do much here.
+1. several pieces of memory are allocated
+2. main is executed
+3. funct1() is called with an arg of 1
+4. Once called the arg is passed to func c
+5. memory is allocated on the heap for a 10 byte str
+6. 5 byte str 'abcde' is copied into str
+7. ends
+*/
