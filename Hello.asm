e059916984b262e0202427853211fbb2a6ff37e5
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2020-07-02 18:38:38 -0700

    Update Hello.asm

--- Hello.asm
+++ Hello.asm
@@ -5,3 +5,13 @@ section .txt                    ; mandatory section declaration
                                 ; exports the entry point to the ELF linker or
 global _start                   ; loaders conveniently recognize _start as the entry point
 _start:
+                                ; write our string to stdout
+                                ; note how arguments will be loaded in reverse
+mov edx, len                    ; third arg message length
+mov ecx, msg                    ; second arg pointer or message to write
+mov ebc, 1                      ; load first arg which is the file stdout
+mov eax, 4                      ; system call number 4 = sys_write
+int 0x80                        ; call kernel interrupt and exit
+mov ebx, 0                      ; load first sys call arg which is the exit code
+mov eax, 1                      ; sys call number 1 = exit
+int 0x80                        ; calls kernel interrup and exits
